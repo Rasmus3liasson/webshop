4d69a73abe645cdef080a519210d6d9a6b7668ca
Rasmus Eliasson <rasmuseliasson@MacBook-Air-som-tillhor-Rasmus.local>
2023-08-11 13:57:58 +0200

    basic about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <>
+      <section className="mx-5 my-4">
+        <h1>Om Oss</h1>
+        <p>
+          Välkommen till oss, din ultimata destination för trendiga kläder och
+          accessoarer. Vi brinner för att erbjuda de senaste trenderna och
+          tidlösa klassikerna för att lyfta din stil.
+        </p>
+        <p>
+          Hos oss tror vi att mode är mer än bara kläder; det är en form av
+          självuttryck. Vår kuraterade kollektion speglar vår dedikation till
+          kvalitet, stil och överkomliga priser.
+        </p>
+        <h2>Vår Mission</h2>
+        <p>
+          Vår mission är att ge individer möjlighet att omfamna sin unika stil
+          genom att erbjuda ett mångsidigt utbud av kläder som passar olika
+          smaker och preferenser. Vi strävar efter att skapa en inkluderande
+          shoppingupplevelse där alla kan hitta något som talar till dem.
+        </p>
+        <h2>Fraktinformation</h2>
+        <p>
+          Vi erbjuder snabb och pålitlig frakt till kunder över hela världen.
+          Beställningar behandlas vanligtvis inom 1-2 arbetsdagar, och du kan
+          förvänta dig att dina varor kommer inom 5-7 arbetsdagar för inrikes
+          beställningar och 7-14 arbetsdagar för internationella beställningar.
+        </p>
+        <p>
+          Fraktkostnader beräknas vid kassan baserat på din plats. Vi
+          tillhandahåller också spårningsinformation för din bekvämlighet, så
+          att du kan hålla koll på ditt pakets resa till din dörr.
+        </p>
+        <h2>Returpolicy</h2>
+        <p>
+          Vi vill att du ska älska ditt köp. Om du av någon anledning inte är
+          helt nöjd med din beställning kan du initiera en retur inom 30 dagar
+          från det att du mottar dina varor. För att vara berättigad till en
+          retur måste varorna vara oanvända, osköljda och i sitt ursprungliga
+          skick med alla etiketter kvar.
+        </p>
+        <p>
+          Kontakta gärna vårt kundsupportteam för att initiera returprocessen.
+          När din retur är godkänd kan du välja att få en återbetalning eller
+          byta ut varan mot en annan storlek eller stil.
+        </p>
+      </section>
+      <style jsx>{`
+        h1,
+        h2 {
+          margin-bottom: 10px;
+        }
+        p {
+          margin-bottom: 20px;
+        }
+      `}</style>
+    </>
+  );
+}

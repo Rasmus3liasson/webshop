b802209c47a60763643b6e8e6f78417385552d04
Rasmus3liasson <ras.muse@hotmail.com>
2024-01-24 15:08:40 +0100

    refactor orderData.ts and init.sql to make post request without a specific order id

--- src/app/utils/orderData.ts
+++ src/app/utils/orderData.ts
@@ -1,78 +1,68 @@
 import { query } from "../../database/db";
 
 export async function getOrderData() {
-  // Destruct and gets first element of array
-  const [data] = (await query({
-    query: "CALL GetOrders()",
-  }));
-  return data.reduce((order: OrderWithProducts[], row: OrderRowsI ) => {
-    const existingOrder = order.find((o) => o.order_id === row.order_id);
+  try {
+    // Destruct and gets first element of array
+    const [data] = await query({
+      query: "CALL GetOrders()",
+    });
+    return data.reduce((order: OrderWithProducts[], row: OrderRowsI) => {
+      const existingOrder = order.find((o) => o.order_id === row.order_id);
 
-    if (existingOrder) {
-      existingOrder.products.push({
-        order_detail_id: row.order_detail_id,
-        product_id: row.product_id,
-        product_name: row.product_name,
-        quantity: row.quantity,
-        product_price: row.product_price,
-        subtotal: row.subtotal,
-      });
-    } else {
-      const newOrder: OrderWithProducts = {
-        order_id: row.order_id,
-        order_date: row.order_date,
-        customer_id: row.customer_id,
-        customer_email: row.customer_email,
-        customer_address: row.customer_address,
-        customer_phone: row.customer_phone,
-        products: [
-          {
-            order_detail_id: row.order_detail_id,
-            product_id: row.product_id,
-            product_name: row.product_name,
-            quantity: row.quantity,
-            product_price: row.product_price,
-            subtotal: row.subtotal,
-          },
-        ],
-      };
+      if (existingOrder) {
+        existingOrder.products.push({
+          order_detail_id: row.order_detail_id,
+          product_id: row.product_id,
+          product_name: row.product_name,
+          quantity: row.quantity,
+          product_price: row.product_price,
+          subtotal: row.subtotal,
+        });
+      } else {
+        const newOrder: OrderWithProducts = {
+          order_id: row.order_id,
+          order_date: row.order_date,
+          customer_id: row.customer_id,
+          customer_email: row.customer_email,
+          customer_address: row.customer_address,
+          customer_phone: row.customer_phone,
+          products: [
+            {
+              order_detail_id: row.order_detail_id,
+              product_id: row.product_id,
+              product_name: row.product_name,
+              quantity: row.quantity,
+              product_price: row.product_price,
+              subtotal: row.subtotal,
+            },
+          ],
+        };
 
-      order.push(newOrder);
-    }
+        order.push(newOrder);
+      }
 
-    return order;
-  }, []);
+      return order.sort((a, b) => a.order_id - b.order_id);
+    }, []);
+  } catch (error) {
+    throw error;
+  }
 }
 export async function addNewOrder(newOrder: OrderWithProducts) {
-  // If the customer already exists, use the existing customer information
-  await query({
-    query: "CALL CustomersAlreadyExist( ?, ?, ? , ?)",
-    values: [
-      newOrder.customer_id,
-      newOrder.customer_email,
-      newOrder.customer_address,
-      newOrder.customer_phone,
-    ],
-  });
+  try {
+    // Convert products array into JSON
+    const productsInJSON = JSON.stringify(newOrder.products);
 
-  // Insert order and get the order id
-  const result = await query({
-    query: "CALL InsertNewCustomer(?)",
-    values: [newOrder.customer_id],
-  });
-
-  const orderId = result?.[0]?.[0]?.insertId;
-
-  // Insert order details
-  for (const product of newOrder.products) {
+    // Insert order details
     await query({
-      query: "CALL InsertProductDetails( ?, ?, ?, ?)",
+      query: "CALL CreateOrder(?, ?, ?, ?)",
       values: [
-        orderId,
-        product.product_id,
-        product.quantity,
-        product.product_price,
+        newOrder.customer_email,
+        newOrder.customer_address,
+        newOrder.customer_phone,
+        productsInJSON,
       ],
     });
+  } catch (error) {
+    throw error;
   }
 }
--- src/database/init.sql
+++ src/database/init.sql
@@ -3,15 +3,15 @@ CREATE DATABASE IF NOT EXISTS web_shop;
 USE web_shop;
 
 CREATE TABLE customers (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(100) NOT NULL,
+    customer_id INTEGER AUTO_INCREMENT PRIMARY KEY UNIQUE ,
+    email VARCHAR(100) NOT NULL UNIQUE ,
     address VARCHAR(255) NOT NULL,
     phone_number VARCHAR(20) NOT NULL
 
 );
 
 CREATE TABLE products (
-    product_id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100),
     price DOUBLE NOT NULL
 );
@@ -36,6 +36,7 @@ CREATE TABLE order_details (
 
 
 -- INSERT EXAMPLE DATA
+
 INSERT INTO customers (email, address, phone_number) VALUES
     ('exempel@exampel.com', 'exempel adress 123', 1234567892),
     ('exempel@exampel.com', 'exempel adress 123', 1234567893),
@@ -67,7 +68,6 @@ SELECT
     c.email AS customer_email,
     c.address AS customer_address,
     c.phone_number AS customer_phone,
-    od.order_detail_id,
     p.product_id,
     p.name AS product_name,
     od.quantity,
@@ -84,6 +84,7 @@ JOIN
 
 
 -- CREATE PROCEDURES
+
 DELIMITER //
 
 CREATE PROCEDURE GetOrders()
@@ -91,33 +92,44 @@ BEGIN
     SELECT * FROM all_orders;
 END //
 
-CREATE PROCEDURE CustomersAlreadyExist(
-    IN p_customer_id INT,
-    IN p_email VARCHAR(100),
-    IN p_address VARCHAR(255),
-    IN p_phone_number VARCHAR(20)
+CREATE PROCEDURE CreateOrder(
+    IN p_customer_email VARCHAR(100),
+    IN p_customer_address VARCHAR(255),
+    IN p_customer_phone_number VARCHAR(20),
+    IN p_products JSON
 )
 BEGIN
-    INSERT IGNORE INTO customers (customer_id, email, address, phone_number)
-    VALUES (p_customer_id, p_email, p_address, p_phone_number);
-END //
+    DECLARE inserted_customer_id INTEGER;
 
-CREATE PROCEDURE InsertNewCustomer(
-    IN p_customer_id INT
-)
-BEGIN
-    INSERT INTO orders (customer_id) VALUES (p_customer_id);
-END //
+    -- Insert/update new customer or get existing customer ID
+    INSERT INTO customers (email, address, phone_number)
+    VALUES (p_customer_email, p_customer_address, p_customer_phone_number)
+    ON DUPLICATE KEY UPDATE customer_id = LAST_INSERT_ID(customer_id);
 
-CREATE PROCEDURE InsertProductDetails(
-    IN p_order_id INT,
-    IN p_product_id INT,
-    IN p_quantity INT,
-    IN p_price DOUBLE
-)
-BEGIN
+    -- Get the customer ID
+    SELECT customer_id INTO inserted_customer_id
+    FROM customers
+    WHERE email = p_customer_email;
+
+    -- Insert the order
+    INSERT INTO orders (customer_id) VALUES (inserted_customer_id);
+
+    -- Insert products
     INSERT INTO order_details (order_id, product_id, quantity, price)
-    VALUES (p_order_id, p_product_id, p_quantity, p_price);
+    SELECT
+        LAST_INSERT_ID(),
+        product.product_id,
+        product.quantity,
+        product.product_price
+    FROM
+        JSON_TABLE(p_products, '$[*]'
+            COLUMNS (
+                product_id INT PATH '$.product_id',
+                quantity INT PATH '$.quantity',
+                product_price DOUBLE PATH '$.product_price'
+            )
+        ) AS product;
 END //
 
 DELIMITER ;
+

63ad1ea58f37c7a93828f013dd344aa6ede183db
Rasmus3liasson <ras.muse@hotmail.com>
2024-07-20 08:03:16 +0200

    Update CategoryList component to fetch categories from API

--- src/app/components/Header/CategoryList.tsx
+++ src/app/components/Header/CategoryList.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 
-import { filterOptionsMock } from "../../../../mockData/items";
+import { useEffect, useState } from "react";
 
 export default function CategoryList({
   setListState: setListState,
@@ -10,10 +10,20 @@ export default function CategoryList({
   setListState: (newState: boolean) => void;
   setNavActive: (newState: boolean) => void;
 }) {
+  const [categories, setCategories] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch("/api/items");
+      const res = await data.json();
+      setCategories(res.filterOptions.categories);
+    };
+    fetchData();
+  }, []);
   return (
     <>
       <ul className="flex flex-col justify-evenly items-center gap-3 animate-slideInNav md:flex-row md:absolute md:top-9 md:w-full md:left-0 w-full md:pt-5">
-        {filterOptionsMock.categories.map((categories, index) => {
+        {categories.map((categories, index) => {
           return (
             <Link
               key={index}
